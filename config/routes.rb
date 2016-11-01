55574f2f3cd5ab3244dc00a069ecce0aa6dd6e37
youngzootopia <youngzootopia@gmail.com>
2016-11-01 14:02:40 +0900

    apache2

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'home/index', to: 'sessions#new'
-  
   #...
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'sessions#new'
+  get	 'home/index',	 to: 'sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  # sign up
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  # sign up, login, logout... etc
+  get	 '/signup',	 to: 'users#new'
+  post	 '/signup',	 to: 'users#create'
+  get	 'sessions/new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
