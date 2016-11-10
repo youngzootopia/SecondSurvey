a8c4c545866e8e5b30ea5549f26a75a9bd5cc77f
youngzootopia <youngzootopia@gmail.com>
2016-11-10 10:43:28 +0900

    remove response controller

--- app/controllers/response_controller.rb
+++
@@ -1,9 +0,0 @@
-class ResponseController < ApplicationController
-  def test
-    @users = User.all
-    request.format = :json
-    respond_to do |format|
-      format.json { render :json => @users }
-    end
-  end
-end
--- test/controllers/response_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ResponseControllerTest < ActionDispatch::IntegrationTest
-  test "should get test" do
-    get response_test_url
-    assert_response :success
-  end
-
-end
